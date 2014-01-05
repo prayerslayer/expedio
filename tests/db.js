3304ce57a7e1b30532986f5afd40e0b562eb2317
Nikolaus Piccolotto <prayerslayer@gmail.com>
2014-01-05 12:57:49 +0100

    unit tests coming!

---
+++ tests/db.js
@@ -0,0 +1,20 @@
+exports.setUp = function( callback ) {
+	//TODO set up code
+
+	callback();
+};
+
+exports.tearDown = function( callback ) {
+	//TODO tear down code
+
+	callback();
+};
+
+exports[ "save unexisting hotel" ] = function( test ) {
+	
+	test.done();
+};
+
+exports[ "update existing hotel" ] = function( test ) {
+	test.done();
+};
